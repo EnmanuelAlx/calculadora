d0b2ab958e57416d33c31d98cb8e5dcfff8bff42
Jeferson <jbalaguera@dichter-neira.com>
2026-03-02 11:52:33 -0500

    Se agrega la función de valor absoluto en operaciones.py y se modifica main.py, esto para tener el codigo por separado

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def suma(a, b):
-    return a + b
+from operaciones import suma, valor_absoluto
 
 if __name__ == "__main__":
     print("Hello, World!")
+    print("Suma:", suma(3, 4))
+    print("Valor absoluto de -5:", valor_absoluto(-5))
+
---
+++ operaciones.py
@@ -0,0 +1,7 @@
+def suma(a, b):
+    return a + b
+
+def valor_absoluto(n):
+    if n < 0:
+        return -n
+    return n
